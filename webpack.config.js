d1e14801a6dc48c22750893ffeb0ef3c778c9fd5
Victor Fernandes <victorfern91@gmail.com>
2017-02-10 17:02:23 +0000

    added stage-0 babel preset
    In order to enable decorators and async/await features from ES7

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ const webpackConfig = {
             exclude: /node_modules/,
             loader: 'babel-loader',
             query: {
-                presets: ['es2015', 'react']
+                presets: ['es2015', 'react', 'stage-0'] // stage-0 enable decorators and async/await features
             }
         }]
     },
